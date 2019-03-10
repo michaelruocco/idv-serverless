b921578a590729b3d295d012c53e44b918b4b58d
Michael Ruocco <michael.ruocco@hotmail.com>
2019-03-10 08:18:07 +0000

    refactors get identity request validator

--- aws-lambda/identity-aws-lambda/src/main/java/uk/co/mruoc/idv/awslambda/identity/GetIdentityRequestValidator.java
+++ aws-lambda/identity-aws-lambda/src/main/java/uk/co/mruoc/idv/awslambda/identity/GetIdentityRequestValidator.java
@@ -16,21 +16,39 @@ public class GetIdentityRequestValidator implements RequestValidator {
     @Override
     public Optional<JsonApiErrorDocument> validate(final APIGatewayProxyRequestEvent input) {
         log.debug("validating request {}", input);
-        final Map<String, String> pathParameters = input.getPathParameters();
-        if (pathParameters != null && pathParameters.containsKey("id")) {
-            log.debug("id path parameter provided, request is valid");
-            return Optional.empty();
-        }
-
-        final Map<String, String> queryStringParameters = input.getQueryStringParameters();
-        if (queryStringParameters != null && queryStringParameters.containsKey("aliasType") && queryStringParameters.containsKey("aliasValue")) {
-            log.debug("aliasType and aliasValue query string parameters provided, request is valid");
+        if (isValid(input)) {
             return Optional.empty();
         }
 
         final JsonApiErrorDocument document = new JsonApiErrorDocument(new IdentityIdOrAliasNotProvidedError());
         log.debug("request is not valid, returning error document {}", document);
         return Optional.of(document);
+    }
+
+    private static boolean isValid(final APIGatewayProxyRequestEvent input) {
+        if (hasIdPathParameter(input)) {
+            log.debug("id path parameter provided, request is valid");
+            return true;
+        }
+
+        if (hasAliasTypeAndValueQueryStringParameters(input)) {
+            log.debug("aliasType and aliasValue query string parameters provided, request is valid");
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean hasIdPathParameter(final APIGatewayProxyRequestEvent input) {
+        final Map<String, String> pathParameters = input.getPathParameters();
+        return pathParameters != null && pathParameters.containsKey("id");
+    }
+
+    private static boolean hasAliasTypeAndValueQueryStringParameters(final APIGatewayProxyRequestEvent input) {
+        final Map<String, String> queryStringParameters = input.getQueryStringParameters();
+        return queryStringParameters != null &&
+                queryStringParameters.containsKey("aliasType") &&
+                queryStringParameters.containsKey("aliasValue");
     }
 
     public static class IdentityIdOrAliasNotProvidedError extends JsonApiErrorItem {
